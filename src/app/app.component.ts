c22d62006c733fbb99f46511a2072702307f49f9
immorale <izteotl.morales@proveedoresgnp.mx>
2023-02-07 13:21:22 -0600

    validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { InteresService } from './services/interes.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  error:string = '';
   gananciaInversion:number = 0;
   montoFinal:number = 0;
   interesCompuestoForm:FormGroup;
@@ -26,16 +27,22 @@ export class AppComponent implements OnInit {
     this.interesCompuestoForm = this.fb.group({
       inversionInicial:['',Validators.required],
       aportacionAnual:['',Validators.required],
-      incrementoAnual:['',Validators.required],
+      incrementoAnual:[''],
       anosInversion:['',Validators.required],
       rendimiento:['',Validators.required],
     });
   }
 
   calcular(){
-    if(this.interesCompuestoForm.valid){      
+    if(this.interesCompuestoForm.valid){ 
+      let incrementoAnual = this.interesCompuestoForm.controls['incrementoAnual'].value;
+      console.log(incrementoAnual);
+      if(!incrementoAnual){
+        incrementoAnual = 0;
+      }
+        
       this.inversion.aportacionAnual = this.interesCompuestoForm.controls['aportacionAnual'].value;
-      this.inversion.incrementoAnual = this.interesCompuestoForm.controls['incrementoAnual'].value;
+      this.inversion.incrementoAnual = incrementoAnual;
       this.inversion.anosDeInversion = this.interesCompuestoForm.controls['anosInversion'].value;
       this.inversion.rendimientoInversion = this.interesCompuestoForm.controls['rendimiento'].value;
       this.inversion.inversionInicial = this.interesCompuestoForm.controls['inversionInicial'].value;
@@ -45,11 +52,13 @@ export class AppComponent implements OnInit {
           this.registros = data.registros;
           this.gananciaInversion = data.gananciaInversion;
           this.montoFinal = data.montoFinal;
+          this.error = '';
         }, error =>{
           console.log(error);
         }
       );
     }
+    this.error ='No es posible procesar su solicitud con los datos proporcionados';
     return;
   }
 }
